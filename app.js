e3cbc004c0ffdcf2c5c9fccaeb55b8e1a086c40a
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2025-05-22 11:57:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -34,16 +34,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/db_article');
 const Article = mongoose.model('Article', { uuid: String, title : String, content : String, author : String }, 'articles');
 
 // --------------------------------------------------------------
-// DATA
-// --------------------------------------------------------------
-// Simulation de données en mémoire
-let articles = [
-    { id: 1, title: 'Premier article', content: 'Contenu du premier article', author: 'Isaac' },
-    { id: 2, title: 'Deuxième article', content: 'Contenu du deuxième article', author: 'Sanchez' },
-    { id: 3, title: 'Troisième article', content: 'Contenu du troisième article', author: 'Toto' }
-];
-
-// --------------------------------------------------------------
 // ROUTES
 // --------------------------------------------------------------
 
